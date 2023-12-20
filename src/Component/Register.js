a9211d6e85bb4252cbeff97a72ff2ce520abf8e5
rajasenthilkumar <raja.senthilkumar@twilightsoftwares.com>
2023-12-20 11:16:35 +0530

    Responsive register code

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -10,7 +10,7 @@ import {
   CFormInput,
 } from '@coreui/react';
 import backgroundImg from '../assets/images/unsplash_MAnVoJlQUvg.png';
-import logo from '../assets/images/evoltec logo-01 1.svg'
+import logo from '../assets/images/evoltec logo-01 1.svg';
 
 const Register = () => {
   const [fullName, setFullName] = useState('');
@@ -75,8 +75,20 @@ const Register = () => {
     validatePassword();
     validateConfirmPassword();
 
-    if (!nameError && !emailError && !phoneError && !passwordError && !confirmPasswordError) {
-      console.log('Registering user:', { fullName, email, phone, password, confirmPassword });
+    if (
+      !nameError &&
+      !emailError &&
+      !phoneError &&
+      !passwordError &&
+      !confirmPasswordError
+    ) {
+      console.log('Registering user:', {
+        fullName,
+        email,
+        phone,
+        password,
+        confirmPassword,
+      });
       // Add your registration logic here
     }
   };
@@ -90,148 +102,138 @@ const Register = () => {
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        minHeight: '1vh',
+        minHeight: '100vh',
       }}
     >
-      <div>
-        <CContainer>
-          <CRow>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-              <img
-                src={logo}
-                alt="Evoltec Logo"
-                style={{ width: '200px', height: '200px' }}
-              />
-            </div>
-            <CCard className="p-4" style={{ width: '500px', borderRadius: '20px', paddingTop: '50px', paddingBottom: '50px' }}>
-              <CCardBody>
-                <div>
-                  <div style={{ marginLeft: '70px' }}>
-                    <p style={{ fontFamily: 'Alfa Slab One', fontSize: '35px', margin: '0', padding: '0', fontWeight: 'bold', textAlign: 'left' }}>Register</p>
-                    <p style={{ fontSize: '18px', color: '#969696', textAlign: 'left' }}>Please register to create your account.</p>
-                  </div>
-                  <CForm style={{ textAlign: 'center' }}>
-                    <div>
-                      <CFormInput
-                        type="text"
-                        id="fullName"
-                        value={fullName}
-                        onBlur={validateName}
-                        onChange={(e) => setFullName(e.target.value)}
-                        placeholder="Full Name"
-                        style={{
-                          width: '350px',
-                          height: '25px',
-                          marginBottom: '15px',
-                          borderRadius: '10px',
-                          border: '2px solid #3300cc',
-                          padding: '10px',
-                        }}
-                      />
-                      {nameError && <p style={{ color: 'red', fontSize: '14px', marginTop: '0' }}>{nameError}</p>}
-                    </div>
-                    <div>
-                      <CFormInput
-                        type="email"
-                        id="email"
-                        value={email}
-                        onBlur={validateEmail}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="E-mail"
-                        style={{
-                          width: '350px',
-                          height: '25px',
-                          marginBottom: '15px',
-                          borderRadius: '10px',
-                          border: '2px solid #3300cc',
-                          padding: '10px',
-                        }}
-                      />
-                      {emailError && <p style={{ color: 'red', fontSize: '14px', marginTop: '0' }}>{emailError}</p>}
-                    </div>
-                    <div>
-                      <CFormInput
-                        type="tel"
-                        id="phone"
-                        value={phone}
-                        onBlur={validatePhone}
-                        onChange={(e) => setPhone(e.target.value)}
-                        placeholder="Phone"
-                        style={{
-                          width: '350px',
-                          height: '25px',
-                          marginBottom: '15px',
-                          borderRadius: '10px',
-                          border: '2px solid #3300cc',
-                          padding: '10px',
-                        }}
-                      />
-                      {phoneError && <p style={{ color: 'red', fontSize: '14px', marginTop: '0' }}>{phoneError}</p>}
-                    </div>
-                    <div>
-                      <CFormInput
-                        type="password"
-                        id="password"
-                        value={password}
-                        onBlur={validatePassword}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                        style={{
-                          width: '350px',
-                          height: '25px',
-                          marginBottom: '15px',
-                          borderRadius: '10px',
-                          border: '2px solid #3300cc',
-                          padding: '10px',
-                        }}
-                      />
-                      {passwordError && <p style={{ color: 'red', fontSize: '14px', marginTop: '0' }}>{passwordError}</p>}
-                    </div>
-                    <div>
-                      <CFormInput
-                        type="password"
-                        id="confirmPassword"
-                        value={confirmPassword}
-                        onBlur={validateConfirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        placeholder="Confirm password"
-                        style={{
-                          width: '350px',
-                          height: '25px',
-                          marginBottom: '15px',
-                          borderRadius: '10px',
-                          border: '2px solid #3300cc',
-                          padding: '10px',
-                        }}
-                      />
-                      {confirmPasswordError && <p style={{ color: 'red', fontSize: '14px', marginTop: '0' }}>{confirmPasswordError}</p>}
-                    </div>
-                    <CButton
-                      onClick={handleRegister}
-                      className="mt-3"
-                      style={{ width: '370px', height: '50px', marginBottom: '20px', backgroundColor: '#8b54ff', color: '#ffffff', borderRadius: '10px', border: 'none' }}
-                    >
-                      Register
-                    </CButton>
-                    <CButton
-                      onClick={handleRegister}
-                      className="mt-3"
-                      style={{ width: '370px', height: '50px', backgroundColor: '#3300cc', color: '#ffffff', borderRadius: '10px', border: 'none' }}
-                    >
-                  <a href="/Sigin" style={{ color: '#ffffff', fontWeight: 'bold', textDecoration: 'none' }}>Have an account? Sign in</a>
-                     
-                    </CButton>
-                  </CForm>
+      <CContainer>
+        <CRow className="justify-content-center align-items-center">
+          <div className="logo-container text-center mb-4">
+            <img
+              src={logo}
+              alt="Evoltec Logo"
+              style={{ width: '150px', height: '150px',marginLeft:'130px' }}
+            />
+          </div>
+          <CCard className="p-4" style={{ width: '100%', borderRadius: '20px', padding: '20px' }}>
+            <CCardBody>
+              <div className="mb-4">
+                <p className="form-title">Register</p>
+                <p className="form-subtitle">
+                  Please register to create your account.
+                </p>
+              </div>
+              <CForm>
+                <div className="mb-3">
+                  <CFormInput
+                    type="text"
+                    id="fullName"
+                    value={fullName}
+                    onBlur={validateName}
+                    onChange={(e) => setFullName(e.target.value)}
+                    placeholder="Full Name"
+                    className="form-input"
+                  />
+                  {nameError && (
+                    <p className="error-message">{nameError}</p>
+                  )}
                 </div>
-              </CCardBody>
-            </CCard>
-          </CRow>
-        </CContainer>
-      </div>
+                <div className="mb-3">
+                  <CFormInput
+                    type="email"
+                    id="email"
+                    value={email}
+                    onBlur={validateEmail}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="E-mail"
+                    className="form-input"
+                  />
+                  {emailError && (
+                    <p className="error-message">{emailError}</p>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <CFormInput
+                    type="tel"
+                    id="phone"
+                    value={phone}
+                    onBlur={validatePhone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    placeholder="Phone"
+                    className="form-input"
+                  />
+                  {phoneError && (
+                    <p className="error-message">{phoneError}</p>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <CFormInput
+                    type="password"
+                    id="password"
+                    value={password}
+                    onBlur={validatePassword}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    className="form-input"
+                  />
+                  {passwordError && (
+                    <p className="error-message">{passwordError}</p>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <CFormInput
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onBlur={validateConfirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    placeholder="Confirm password"
+                    className="form-input"
+                  />
+                  {confirmPasswordError && (
+                    <p className="error-message">{confirmPasswordError}</p>
+                  )}
+                </div>
+                <CButton
+                  onClick={handleRegister}
+                  className="mt-3 register-button"
+                  style={{
+                    backgroundColor: '#8b54ff',
+                    color: '#ffffff',
+                    borderRadius: '10px',
+                    border: 'none',
+                    marginBottom: '10px',
+                  }}
+                >
+                  Register
+                </CButton>
+                <div style={{ textAlign: 'center' }}>
+                  <CButton
+                    onClick={handleRegister}
+                    className="register-button"
+                    style={{
+                      backgroundColor: '#3300cc',
+                      color: '#ffffff',
+                      borderRadius: '10px',
+                      border: 'none',
+                    }}
+                  >
+                    <a
+                      href="/Signin"
+                      style={{
+                        color: '#ffffff',
+                        fontWeight: 'bold',
+                        textDecoration: 'none',
+                      }}
+                    >
+                      Have an account? Sign in
+                    </a>
+                  </CButton>
+                </div>
+              </CForm> 
+            </CCardBody>
+          </CCard>
+        </CRow>
+      </CContainer>
     </div>
   );
 };
